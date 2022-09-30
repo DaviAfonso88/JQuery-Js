ea8a9b971c0d0723f1cb27de0f0d9bd2b24ce36d
DaviAfonso88 <daviafonso5894@gmail.com>
2022-09-30 11:40:23 -0300

    segundo commit

--- js/aula/05.js
+++ js/aula/05.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-    //descendente
-    $("div span").css("background-color", "yellow");
-    //filhos do elemento
-    $("li > ul").css("color", "red");
-    //irmãos do elemento
-    $("#Item1_1 + li").css("color", "blue");
-    //todos os irmãos do elemnto
-    $("#Item1_1 ~ li").css("color", "blue");
-   
+    //$("table tr").css("background-color", "#CCC");
+    //$("table tr:not(tr:last) and (tr:first)").css("background-color", "#CCC");
+    //$("table tr:odd").css("background-color", "#CCC");
+    $("table tr:even").css("background-color", "#CCC");
+    $("table tr:first").css("background-color", "black");
+    //posição
+    $("table tr:eq(1)").css("background-color", "red");
+    $("table tr:eq(3)").css("background-color", "red");
+    //posição maior
+    //$("table tr:gt(2)").css("background-color", "red");
+    //posição menor
+    //$("table tr:lt(2)").css("background-color", "red");
+    
 });
