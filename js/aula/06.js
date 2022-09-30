d8d7b53f78144a0a81e925d89d07b04854b75934
DaviAfonso88 <daviafonso5894@gmail.com>
2022-09-30 13:05:15 -0300

    segundo

--- js/aula/06.js
+++ js/aula/06.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     $("td:not(td:empty)").css("background-color", "yellow");
     $("table tr:first").css("background-color", "black");
     $("td:has(strong)").css("background-color", "green");
+    $("td:empty").css("background-color", "blue");
 
     
     
