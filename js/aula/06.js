dc0268d545e5efd245b4d6239bc449cd652d4277
DaviAfonso88 <daviafonso5894@gmail.com>
2022-09-30 12:01:50 -0300

    terceiro commit

---
+++ js/aula/06.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("td:contains(Davi)").css("background-color", "green");
+  
+    //celulas vazias
+    $("td:empty").css("background-color", "green");
+    $("td:not(td:empty)").css("background-color", "yellow");
+    $("table tr:first").css("background-color", "black");
+    $("td:has(strong)").css("background-color", "green");
+
+    
+});
