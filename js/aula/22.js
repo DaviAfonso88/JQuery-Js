702a21808e448cea31208c2eca1ef29bbfa4e1a6
DaviAfonso88 <daviafonso5894@gmail.com>
2022-10-13 14:46:18 -0300

    up

--- js/aula/22.js
+++ js/aula/22.js
@@ -11,6 +11,18 @@ $(document).ready(function(){
 
     })
 
+    $(":input").bind("keydown", function(e){
+        var keyCode = e.which;
+        var isStandard = (keyCode >47 && keyCode < 58)
+        var isOther = (",8,46,37,38,39,40,".indexOf(","+keyCode+",") > -1);
+        if(isStandard | isOther){
+            return true;
+        } else{
+            return false;
+        }
+
+    })
+
 
 
     
